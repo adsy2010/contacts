165ad0a79cd94054bf6d90e0f6046e8f02fa1d6e
adsy2010 <adsy2010@users.noreply.github.com>
2022-07-02 22:52:46 +0100

    Added store contact request with rules

---
+++ app/Http/Requests/StoreContactRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        //todo: add a policy
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'date_of_birth' => 'nullable|date',
+            'email' => 'nullable|email',
+        ];
+    }
+}
