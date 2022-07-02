bfe034df800a6c1d354083ce33a281fc23c1eb0b
adsy2010 <adsy2010@users.noreply.github.com>
2022-07-02 22:52:27 +0100

    Cleaned up classes with imports Added title and breadcrumbs to index, create, edit and show methods Added save on update method, removed id from contact as not required on redirect Added a store contact request to store method

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,18 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreContactRequest;
 use App\Models\Contact;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class ContactController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Inertia\Response
+     * @return Response
      */
-    public function index(): \Inertia\Response
+    public function index(): Response
     {
         $contacts = Contact::query()
             ->withCount(['addresses', 'phoneNumbers'])
@@ -21,6 +24,10 @@ class ContactController extends Controller
 
 
         return Inertia::render('Contact/Index', [
+            'title' => "Contacts",
+            'breadcrumbs' => [
+                ['text' => 'Contacts', 'href' => '/contact', 'active' => true]
+            ],
             'contacts' => $contacts,
             'can' => [],
             'filters' => []
@@ -30,11 +37,16 @@ class ContactController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Inertia\Response
+     * @return Response
      */
-    public function create(): \Inertia\Response
+    public function create(): Response
     {
         return Inertia::render('Contact/Create', [
+            'title' => "Create contact",
+            'breadcrumbs' => [
+                ['text' => 'Contacts', 'href' => '/contact', 'active' => false],
+                ['text' => "Create contact", 'href' => "/contact/create", 'active' => true],
+            ],
             'can' => [],
             'filters' => [],
         ]);
@@ -43,10 +55,10 @@ class ContactController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
+     * @param StoreContactRequest $request
+     * @return RedirectResponse
      */
-    public function store(Request $request): \Illuminate\Http\RedirectResponse
+    public function store(StoreContactRequest $request): RedirectResponse
     {
         Contact::query()->create($request->all());
 
@@ -56,13 +68,18 @@ class ContactController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Contact  $contact
-     * @return \Inertia\Response
+     * @param Contact $contact
+     * @return Response
      */
-    public function show(Contact $contact): \Inertia\Response
+    public function show(Contact $contact): Response
     {
-        return Inertia::render('Contact/Create', [
+        return Inertia::render('Contact/Show', [
             'contact' => $contact->load('addresses', 'phoneNumbers'),
+            'title' => "{$contact->first_name} {$contact->last_name}",
+            'breadcrumbs' => [
+                ['text' => 'Contacts', 'href' => '/contact', 'active' => false],
+                ['text' => "{$contact->first_name} {$contact->last_name}", 'href' => "/contact/{$contact->id}", 'active' => true],
+            ],
             'can' => [],
             'filters' => [],
         ]);
@@ -71,13 +88,19 @@ class ContactController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Contact  $contact
-     * @return \Inertia\Response
+     * @param Contact $contact
+     * @return Response
      */
-    public function edit(Contact $contact): \Inertia\Response
+    public function edit(Contact $contact): Response
     {
         return Inertia::render('Contact/Edit', [
             'contact' => $contact->load('addresses', 'phoneNumbers'),
+            'title' => "{$contact->first_name} {$contact->last_name}",
+            'breadcrumbs' => [
+                ['text' => 'Contacts', 'href' => '/contact', 'active' => false],
+                ['text' => "{$contact->first_name} {$contact->last_name}", 'href' => "/contact/{$contact->id}", 'active' => false],
+                ['text' => "Edit", 'href' => "/contact/{$contact->id}/edit", 'active' => true],
+            ],
             'can' => [],
             'filters' => [],
         ]);
@@ -86,24 +109,24 @@ class ContactController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Contact  $contact
-     * @return \Illuminate\Http\RedirectResponse
+     * @param Request $request
+     * @param Contact $contact
+     * @return RedirectResponse
      */
-    public function update(Request $request, Contact $contact): \Illuminate\Http\RedirectResponse
+    public function update(Request $request, Contact $contact): RedirectResponse
     {
-        $contact->fill($request->all());
+        $contact->fill($request->all())->save();
 
-        return redirect()->to(route('contact.show', ['contact' => $contact->id]));
+        return redirect()->to(route('contact.show', ['contact' => $contact]));
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Contact  $contact
-     * @return \Illuminate\Http\RedirectResponse
+     * @param Contact $contact
+     * @return RedirectResponse
      */
-    public function destroy(Contact $contact): \Illuminate\Http\RedirectResponse
+    public function destroy(Contact $contact): RedirectResponse
     {
         $contact->delete();
 
