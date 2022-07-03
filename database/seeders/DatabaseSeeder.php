80ff80417fb87e89b33c50c85773c3f160cedac0
adsy2010 <adsy2010@users.noreply.github.com>
2022-07-03 23:21:48 +0100

    Added seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ContactSeeder::class);
+        $this->call(AddressSeeder::class);
+        $this->call(PhoneNumberSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
