40d809e70a6e9a30c1ae35df9b0d6850962da83f
adsy2010 <adsy2010@users.noreply.github.com>
2022-07-03 23:23:38 +0100

    Added delete button for deleting a contact with delete method

--- resources/js/Pages/Contact/Partials/Details.vue
+++ resources/js/Pages/Contact/Partials/Details.vue
@@ -1,4 +1,6 @@
 <script setup>
+import {TrashIcon} from '@heroicons/vue/outline';
+import {useForm} from '@inertiajs/inertia-vue3'
 import PersonalInformation from "./PersonalInformation";
 import NotificationPreferences from "./NotificationPreferences";
 import Profile from "./Profile";
@@ -11,6 +13,11 @@ let props = defineProps({
     profile: Boolean,
     contact: Object
 });
+
+let deleteContact = () => {
+    let form = useForm({});
+    form.delete(`/contact/${props.contact.id}`);
+}
 
 </script>
 
@@ -27,5 +34,14 @@ let props = defineProps({
         <Divider v-if="(profile || personalInformation) && notificationPreferences" />
 
         <NotificationPreferences v-if="notificationPreferences" :contact="contact"></NotificationPreferences>
+
+        <!-- Delete contact -->
+        <div class="my-6 flex justify-end pr-2 xl:pr-0">
+            <button
+                @click="deleteContact"
+                class="flex items-center bg-red-600 rounded-md text-white hover:bg-red-800 px-3 py-2">
+                <TrashIcon class="h-5 w-5"/><span class="pl-2"> Delete Contact</span>
+            </button>
+        </div>
     </div>
 </template>
