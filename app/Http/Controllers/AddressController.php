6a9ca3cd383817319b8cfdf926d0f31861bbc669
adsy2010 <adsy2010@users.noreply.github.com>
2022-07-03 23:22:29 +0100

    Updated controller with create, store, edit, update and destroy methods

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Address;
+use App\Models\Contact;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class AddressController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
+     * @param Contact $contact
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Contact $contact)
     {
         //
     }
@@ -20,31 +23,45 @@ class AddressController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Inertia\Response
      */
-    public function create()
+    public function create(Contact $contact)
     {
-        //
+        return Inertia::render('Contact/Address/Create', [
+            'contact' => $contact,
+            'title' => 'Add Address',
+            'breadcrumbs' => [
+                ['text' => 'Contacts', 'href' => '/contact', 'active' => false],
+                ['text' => "{$contact->first_name} {$contact->last_name}", 'href' => "/contact/{$contact->id}", 'active' => false],
+                ['text' => "Add Address", 'href' => "/contact/{$contact->id}/address/create", 'active' => true],
+            ],
+            'can' => [],
+            'filters' => [],
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param Contact $contact
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request)
+    public function store(Request $request, Contact $contact)
     {
-        //
+        $address = Address::query()->create(array_merge($request->all(), ['contact_id' => $contact->id]));
+
+        return redirect()->to(route('contact.show', ['contact' => $contact]));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Address  $address
+     * @param Contact $contact
+     * @param \App\Models\Address $address
      * @return \Illuminate\Http\Response
      */
-    public function show(Address $address)
+    public function show(Contact $contact, Address $address)
     {
         //
     }
@@ -52,34 +69,52 @@ class AddressController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Address  $address
-     * @return \Illuminate\Http\Response
+     * @param Contact $contact
+     * @param \App\Models\Address $address
+     * @return \Inertia\Response
      */
-    public function edit(Address $address)
+    public function edit(Contact $contact, Address $address)
     {
-        //
+        return Inertia::render('Contact/Address/Edit', [
+            'contact' => $contact,
+            'address' => $address,
+            'title' => 'Update Address',
+            'breadcrumbs' => [
+                ['text' => 'Contacts', 'href' => '/contact', 'active' => false],
+                ['text' => "{$contact->first_name} {$contact->last_name}", 'href' => "/contact/{$contact->id}", 'active' => false],
+                ['text' => "Update Address", 'href' => "/contact/{$contact->id}/address/{$address->id}/edit", 'active' => true],
+            ],
+            'can' => [],
+            'filters' => [],
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Address  $address
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param Contact $contact
+     * @param \App\Models\Address $address
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, Address $address)
+    public function update(Request $request, Contact $contact, Address $address)
     {
-        //
+        $address->fill($request->all())->save();
+
+        return redirect()->to(route('contact.show', ['contact' => $contact]));
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Address  $address
-     * @return \Illuminate\Http\Response
+     * @param Contact $contact
+     * @param \App\Models\Address $address
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function destroy(Address $address)
+    public function destroy(Contact $contact, Address $address)
     {
-        //
+        $address->delete();
+
+        return redirect()->to(route('contact.show', ['contact' => $contact]));
     }
 }
--- app/Http/Controllers/PhoneNumbersController.php
+++ app/Http/Controllers/PhoneNumbersController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use App\Models\PhoneNumber;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class PhoneNumbersController extends Controller
 {
@@ -20,28 +24,42 @@ class PhoneNumbersController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param Contact $contact
+     * @return Response
      */
-    public function create()
+    public function create(Contact $contact)
     {
-        //
+        return Inertia::render('Contact/PhoneNumber/Create', [
+            'contact' => $contact,
+            'title' => 'Add Phone Number',
+            'breadcrumbs' => [
+                ['text' => 'Contacts', 'href' => '/contact', 'active' => false],
+                ['text' => "{$contact->first_name} {$contact->last_name}", 'href' => "/contact/{$contact->id}", 'active' => false],
+                ['text' => "Add Phone Number", 'href' => "/contact/{$contact->id}/phoneNumber/create", 'active' => true],
+            ],
+            'can' => [],
+            'filters' => [],
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Contact $contact
+     * @return RedirectResponse
      */
-    public function store(Request $request)
+    public function store(Request $request, Contact $contact)
     {
-        //
+        $phoneNumber = PhoneNumber::query()->create(array_merge($request->all(), ['contact_id' => $contact->id]));
+
+        return redirect()->to(route('contact.show', ['contact' => $contact]));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\PhoneNumber  $phoneNumbers
+     * @param PhoneNumber $phoneNumbers
      * @return \Illuminate\Http\Response
      */
     public function show(PhoneNumber $phoneNumbers)
@@ -52,34 +70,52 @@ class PhoneNumbersController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\PhoneNumber  $phoneNumbers
-     * @return \Illuminate\Http\Response
+     * @param Contact $contact
+     * @param PhoneNumber $phoneNumber
+     * @return Response
      */
-    public function edit(PhoneNumber $phoneNumbers)
+    public function edit(Contact $contact, PhoneNumber $phoneNumber)
     {
-        //
+        return Inertia::render('Contact/PhoneNumber/Edit', [
+            'contact' => $contact,
+            'phoneNumber' => $phoneNumber,
+            'title' => 'Update Phone Number',
+            'breadcrumbs' => [
+                ['text' => 'Contacts', 'href' => '/contact', 'active' => false],
+                ['text' => "{$contact->first_name} {$contact->last_name}", 'href' => "/contact/{$contact->id}", 'active' => false],
+                ['text' => "Update Phone Number", 'href' => "/contact/{$contact->id}/phoneNumber/{$phoneNumber->id}/edit", 'active' => true],
+            ],
+            'can' => [],
+            'filters' => [],
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\PhoneNumber  $phoneNumbers
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Contact $contact
+     * @param PhoneNumber $phoneNumber
+     * @return RedirectResponse
      */
-    public function update(Request $request, PhoneNumber $phoneNumbers)
+    public function update(Request $request, Contact $contact, PhoneNumber $phoneNumber)
     {
-        //
+        $phoneNumber->fill($request->all())->save();
+
+        return redirect()->to(route('contact.show', ['contact' => $contact]));
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\PhoneNumber  $phoneNumbers
-     * @return \Illuminate\Http\Response
+     * @param Contact $contact
+     * @param PhoneNumber $phoneNumber
+     * @return RedirectResponse
      */
-    public function destroy(PhoneNumber $phoneNumbers)
+    public function destroy(Contact $contact, PhoneNumber $phoneNumber): RedirectResponse
     {
-        //
+        $phoneNumber->delete();
+
+        return redirect()->to(route('contact.show', ['contact' => $contact]));
     }
 }
